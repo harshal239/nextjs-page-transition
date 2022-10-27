756dadeeda8f649c5134b92e1d3cc24899048ce4
harshal239 <harshalwalunj49@gmail.com>
2022-10-27 23:15:30 +0530

    Added animation using framer-motion

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,28 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { useRouter } from "next/router";
 export default function Navbar() {
+  const router = useRouter();
   return (
-    <nav>
-      <ul>
-        <Link></Link>
+    <nav className="text-2xl font-medium relative z-20 ">
+      <ul className="flex gap-12">
+        <Link href="/">
+          <li
+            className={`cursor-pointer ${
+              router.pathname == "/" ? "underline" : ""
+            }`}
+          >
+            Home
+          </li>
+        </Link>
+        <Link href="/contact">
+          <li
+            className={`cursor-pointer ${
+              router.pathname == "/contact" ? "underline" : ""
+            }`}
+          >
+            Contacts
+          </li>
+        </Link>
       </ul>
     </nav>
   );
